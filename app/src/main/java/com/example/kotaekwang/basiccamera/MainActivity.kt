a7e91bd6d7cedf2ee08a28cdbde115ce70e2d1d3
고태광 <kotaekwang@192.168.nate.com>
2019-03-08 21:29:15 +0900

    complete code -save image

--- app/src/main/java/com/example/kotaekwang/basiccamera/MainActivity.kt
+++ app/src/main/java/com/example/kotaekwang/basiccamera/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.kotaekwang.basiccamera
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.hardware.Camera
+import android.media.ExifInterface
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
@@ -38,26 +44,44 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun takePicture(){
-        Toast.makeText(this,"TAKEPICTURE",Toast.LENGTH_SHORT).show()
         mCamera?.takePicture(null,null,mPicture)
     }
 
     private val mPicture = Camera.PictureCallback { data, _ ->
 
         val pictureFile: File = getOutputMediaFile(MEDIA_IMAGE_TYPE) ?: run {
-            Toast.makeText(this@MainActivity,"fail make file",Toast.LENGTH_SHORT).show()
         Log.d("BasicCamera", ("Error creating media file, check storage permissions"))
         return@PictureCallback
         }
 
         try{
             val fos = FileOutputStream(pictureFile)
-            fos.write(data)
+
+            var realImage : Bitmap = BitmapFactory.decodeByteArray(data, 0, data.size)
+            var exif  = ExifInterface(pictureFile.toString())
+
+            Log.d("EXIF value", exif.getAttribute(ExifInterface.TAG_ORIENTATION))
+
+            if(exif.getAttribute(ExifInterface.TAG_ORIENTATION).equals("6")){
+                realImage = rotate(realImage,90f)
+            }else if(exif.getAttribute(ExifInterface.TAG_ORIENTATION).equals("8")){
+                realImage = rotate(realImage,270f)
+            }else if(exif.getAttribute(ExifInterface.TAG_ORIENTATION).equals("3")){
+                realImage = rotate(realImage,180f)
+            }else if(exif.getAttribute(ExifInterface.TAG_ORIENTATION).equals("0")){
+                realImage = rotate(realImage,90f)
+            }
+            realImage.compress(Bitmap.CompressFormat.JPEG,100,fos)
+            //fos.write(data)
             fos.close()
+            sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,Uri.parse("file://$pictureFile")))
+
         }catch (e: FileNotFoundException){
+            Toast.makeText(this,"file not found : ${e.message}",Toast.LENGTH_SHORT).show()
             Log.d("BasicCamera", "File not found: ${e.message}")
         }catch (e: IOException){
-            Log.d("BasicCamera", "File not found: ${e.message}")
+            Toast.makeText(this,"IOException : ${e.message}",Toast.LENGTH_SHORT).show()
+            Log.d("BasicCamera", "IOException: ${e.message}")
         }
     }
 
@@ -106,15 +130,15 @@ class MainActivity : AppCompatActivity() {
         // To be safe, you should check that the SDCard is mounted
         // using Environment.getExternalStorageState() before doing this.
 
-        val mediaStorageDir = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"basicCameraApp")
+        val mediaStorageDir = File(Environment.
+                getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"basicCameraApp")
         // This location works best if you want the created images to be shared
         // between applications and persist after your app has been uninstalled.
-        Toast.makeText(this@MainActivity,"GETOUT$mediaStorageDir",Toast.LENGTH_SHORT).show()
+
         // Create the storage directory if it does not exist
         mediaStorageDir.apply {
             if(!exists()){
                 if(!mkdirs()){
-                    Toast.makeText(this@MainActivity,"fail create",Toast.LENGTH_SHORT).show()
                     Log.d("BasicCamera", "failed to create directory")
                     return null
                 }
@@ -123,7 +147,17 @@ class MainActivity : AppCompatActivity() {
 
         //Create file name
         val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        return File("${mediaStorageDir.path}${File.separator}IMG_$timeStamp")
+        return File("${mediaStorageDir.absolutePath}${File.separator}IMG_$timeStamp.jpg")
+    }
+
+    private fun rotate(bitmap:Bitmap , degree:Float) : Bitmap{
+        val width = bitmap.width
+        val height = bitmap.height
+
+        val mtx = Matrix()
+        mtx.setRotate(degree)
+
+        return Bitmap.createBitmap(bitmap,0,0,width,height,mtx,true)
     }
 
     @Override
